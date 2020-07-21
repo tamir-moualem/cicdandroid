aa98d17196c781d037ee84c02cf4a9f116891391
Tamir Mualem <Tamir.Mualem@vonage.com>
2020-07-21 09:23:36 +0300

    test commit

--- app/src/androidTest/java/com/example/cicdandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/cicdandroid/ExampleInstrumentedTest.kt
@@ -4,15 +4,11 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
