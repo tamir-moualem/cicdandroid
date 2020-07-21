139c8049c8e33db807b903a7de8fc96933c7097a
Tamir Mualem <Tamir.Mualem@vonage.com>
2020-07-21 09:14:46 +0300

    Adding failed tests

--- app/src/androidTest/java/com/example/cicdandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/cicdandroid/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.example.cicdandroid
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,15 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
+    fun failedTest() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.cicdandroid", appContext.packageName)
+        onView(withId(R.id.cicd2))
+            // Should be CICD2
+            .check(matches(withText("Hello CICD")))
     }
 }
